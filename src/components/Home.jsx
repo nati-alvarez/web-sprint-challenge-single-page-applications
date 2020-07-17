9226f9fa1ad36409be135b5f5c2e8ffb14096f1d
Natividad Alvarez <tivialvarez@gmail.com>
2020-07-17 19:47:22 -0400

    Created basic markup and styling for Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
+import "../styles/home.css";
+
 export default function Home(props){
     return (
         <div className="home">
-            
+            <section className="pizza-delivery">
+                <h1>Your Favorite Food Delivered While Coding</h1>
+                <button>Pizza?</button>
+            </section>
         </div>
     )
 }
