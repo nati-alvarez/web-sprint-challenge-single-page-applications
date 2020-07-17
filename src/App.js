d241fd9715794cf916f4b5eafddde28ade78b147
Natividad Alvarez <tivialvarez@gmail.com>
2020-07-17 19:38:21 -0400

    Created routing for components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
-import Header from "./components/Header"
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Form from "./components/Form";
 
 const App = () => {
   return (
     <>
      <Header/>
+     <Router>
+       <Route exact path="/" component={Home}/>
+       <Route path="/pizza" component={Form}/>
+     </Router>
     </>
   );
 };
