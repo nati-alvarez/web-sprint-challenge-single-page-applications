f59da1e2933fb41f5ad2505d646f528794daec26
Natividad Alvarez <tivialvarez@gmail.com>
2020-07-17 20:23:10 -0400

    Created form markup and styles

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,60 @@
 import React from "react";
 
+import "../styles/form.css";
+
 export default function Form(props){
     return (
         <div className="build-pizza">
-            
+            <form>
+                <img width="100%" src="https://images7.alphacoders.com/690/690886.jpg"/>
+                <h1>Build Your Pizza</h1>
+                <section>
+                    <div className="form-heading">
+                        <h2>Enter Your Name</h2>
+                        <small>required</small>
+                    </div>
+                    <label htmlFor="name">Name: </label>
+                    <input id="name" name="name" type="text"/>
+                </section>
+                <section>
+                    <div className="form-heading">
+                        <h2>Select a Size</h2>
+                        <small>required</small>
+                    </div>
+                    <label htmlFor="size">Size: </label>
+                    <select id="size" name="size">
+                        <option value="small">Small</option>
+                        <option value="medium">Medium</option>
+                        <option value="large">Large</option>
+                    </select>
+                </section>
+                <section>
+                    <div className="form-heading">
+                        <h2>Toppings</h2>
+                        <small>choose up to 4</small>
+                    </div>
+                    <label htmlFor="pepperoni">
+                       Pepperoni <input name="pepperoni" id="pepperoni" type="checkbox"/>
+                    </label>
+                    <label htmlFor="olives">
+                       Olives <input name="olives"  id="olives" type="checkbox"/>
+                    </label>
+                    <label htmlFor="ham">
+                       Ham <input name="ham"  id="ham" type="checkbox"/>
+                    </label>
+                    <label htmlFor="onion">
+                       Onion <input name="onion"  id="onion" type="checkbox"/>
+                    </label>
+                </section>
+                <section>
+                    <div className="form-heading">
+                        <h2>Anything Else?</h2>
+                    </div>
+                    <label htmlFor="specialInstructions">Special instructions: </label>
+                    <input name="specialInstructions" id="specialInstructions" type="text"/>
+                </section>
+                <button>Add to Order</button>
+            </form>
         </div>
     )
 }
